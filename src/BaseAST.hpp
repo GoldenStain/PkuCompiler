29b01b955d1b67bedc79818759916a5ae75da825
DJmachine <goldenstainxym@gmail.com>
2023-10-12 09:14:10 +0800

    src

---
+++ src/BaseAST.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+class BaseAST
+{
+public:
+    virtual ~BaseAST() = default;
+    virtual void Dump() const = 0;
+};
+
+class CompUnitAST : public BaseAST
+{
+public:
+    std::unique_ptr<BaseAST> func_def;
+    void Dump() const override
+    {
+        std::cout << "CompUnitAST { ";
+        func_def->Dump();
+        std::cout << "}";
+    }
+};
+
+class FuncDefAST : public BaseAST
+{
+public:
+    std::unique_ptr<BaseAST> func_type;
+    std::string ident;
+    std::unique_ptr<BaseAST> block;
+    void Dump() const override
+    {
+        std::cout << "FuncDefAST { ";
+        func_type->Dump();
+        std::cout << ", " << ident << ", ";
+        block->Dump();
+        std::cout << " }";
+    }
+};
+
+class FuncTypeAST : public BaseAST
+{
+public:
+    std:: string mytype;
+    void Dump() const override
+    {
+        std::cout << "FuncTypeAST { ";
+        std::cout << mytype;
+        std::cout << " }";
+    }
+};
+
+class BlockAST : public BaseAST
+{
+public:
+    std:: unique_ptr<BaseAST> stmt;
+    void Dump() const override 
+    {
+        std::cout << "BlockAST { ";
+        stmt->Dump();
+        std::cout << " }";
+    }
+};
+
+class StmtAST : public BaseAST
+{
+public:
+    int number;
+    void Dump() const override 
+    {
+        std::cout << "StmtAST { ";
+        std::cout << "return ";
+        std::cout << number;
+        std::cout << " ; }";
+    }
+};
+
